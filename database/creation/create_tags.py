db68a54035c575c6fa30df48560b5c43d226b3e5
pizzey <kjpizzey@gmail.com>
2014-07-05 20:43:50 +0100

    Re-structured the database creation scripts

---
+++ database/creation/create_tags.py
@@ -0,0 +1,43 @@
+from pymongo import MongoClient
+from lxml import etree
+from dateutil.parser import parse
+
+from time import gmtime, strftime
+import os.path
+import os
+
+data_dir = '../../so_data'
+file_name = 'Tags.xml'
+db_name = 'nidaba'
+coll_name = 'tags'
+
+client = MongoClient()
+db = client[db_name]
+coll = db[coll_name]
+
+context = etree.iterparse(os.path.join(data_dir, file_name), 
+                          events=('start', 'end'))
+
+str_to_int = {'Id', 'Count', 'ExcerptPostId', 'WikiPostId'}
+
+f = open('./logs/{:s}.log'.format(coll_name), 'w')
+f.write('Importing {:s} data.\n\n'.format(coll_name))
+
+for i, (event, elem) in enumerate(context):
+    if event == 'end' and elem.tag == 'row':
+        # Create a dictionary and convert any necessary fields.
+        d = {k:int(v) if k in str_to_int else
+             v for k, v in elem.items()}
+        coll.insert(d)
+        elem.clear()
+        while elem.getprevious() is not None:
+            del elem.getparent()[0]
+        if i % 1000 == 0:
+            s_option = (strftime('%H:%M:%S', gmtime()), d['Id'])
+            s = '{:s} : Id - {:d}\n'.format(*s_option)
+            print(s, end='')
+            f.write(s)
+
+coll.ensure_index('Id')
+
+f.close()
---
+++ database/creation/create_votes.py
@@ -0,0 +1,61 @@
+from pymongo import MongoClient
+from lxml import etree
+from dateutil.parser import parse
+
+import pickle
+from time import gmtime, strftime
+import os.path
+import os
+
+data_dir = '../../so_data'
+file_name = 'Votes.xml'
+db_name = 'nidaba'
+coll_name = 'votes'
+
+client = MongoClient()
+db = client[db_name]
+coll = db[coll_name]
+
+context = etree.iterparse(os.path.join(data_dir, file_name), 
+                          events=('start', 'end'))
+
+str_to_int = {'Id', 'PostId', 'VoteTypeId'}
+str_to_date = {'CreationDate'}
+
+# Load in a set of python ids.
+with open('question_ids.pickle', 'rb') as q, \
+     open('answer_ids.pickle', 'rb') as a:
+    question_ids = pickle.load(q)
+    answer_ids = pickle.load(a)
+    ids = question_ids | answer_ids
+
+f = open('./logs/{:s}.log'.format(coll_name), 'w')
+s = 'Importing {:s} data.\n\n'.format(coll_name)
+f.write(s)
+print(s, end='')
+
+i = 0
+for event, elem in context:
+    if event == 'end' and elem.tag == 'row':
+        # Create a dictionary and convert any necessary fields.
+        d = dict(elem.items())
+        if int(d['PostId']) in ids:
+            d = {k:int(v) if k in str_to_int else
+                 parse(v) if k in str_to_date else
+                 v for k, v in d.items()}
+            coll.insert(d)
+            elem.clear()
+            while elem.getprevious() is not None:
+                del elem.getparent()[0]
+            i += 1
+            if i % 50000 == 0:
+                s_option = (strftime('%H:%M:%S', gmtime()), d['Id'], i)
+                s = '{:s} : Id - {:d} : # - {:d}\n'.format(*s_option)
+                print(s, end='')
+                f.write(s)
+
+print('Creating indices.')
+
+coll.ensure_index('Id')
+
+f.close()
