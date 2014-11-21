8b24bb48585df72dd762d32f2dcc8362a5cb0ddb
Ffisegydd <kjpizzey@gmail.com>
2014-11-21 19:58:06 +0000

    Moved objects from individual files into an objects file.

--- nidaba/core/objects.py
+++ nidaba/core/objects.py
@@ -1,4 +1,16 @@
-from .answer import Answer
+class Answer(object):
+    """
+    Stack Overflow Answer object which will hold information for use in Nidaba analysis.
+    """
+
+    def __init__(self, data):
+        """
+        :param data: dict containing Answer information.
+        :return: None
+        """
+
+        self.__data = data
+
 
 class Question(object):
     """
