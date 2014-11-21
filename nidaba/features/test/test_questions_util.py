9cdf649a67a111cd8a0ef0a6f98678223a611fdb
Ffisegydd <kjpizzey@gmail.com>
2014-11-21 16:24:22 +0000

    Removed fixtures
    Have removed fixtures from being used in tests, but left the json files there for now as the data will come in useful in other tests.

--- nidaba/features/test/test_questions_util.py
+++ nidaba/features/test/test_questions_util.py
@@ -1,15 +1,20 @@
-from .fixtures import question_fixture
 from .._util import question
 
 ### General
 
 ### Title
 
-def test_title_capitalisation(question_fixture):
-    assert question.capitalised_title(question_fixture['Title'])
+def test_title_capitalisation():
+    assert question.capitalised_title('Why is reading lines from stdin much slower in C++ than Python?')
+    assert not question.capitalised_title('how make pysmp oids respone readable for human')
+    assert not question.capitalised_title('??? wat do???')
 
-def test_title_capitalisation_ratio(question_fixture):
-    assert question.title_capitalisation_ratio(question_fixture['Title']) == (3./46.)
+def test_title_capitalisation_percentage():
+    assert question.title_capitalisation_percentage('Why is reading lines from stdin much slower in C++ than Python?') == 3/(46 + 3)
+    assert question.title_capitalisation_percentage('how make pysmp oids respone readable for human') == (0/(39 + 0))
+    assert question.title_capitalisation_percentage('WAT DO PYTHON') == 11/(0 + 11)
+    assert question.title_capitalisation_percentage('Matplotlib: I need SOME HELP!!!') == 10/(13 + 10)
+
 
 ### Body
 
