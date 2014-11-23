63f5b03b6c3d32f16c1ca403e65c27837042e9bb
chillaranand <anand21nanda@gmail.com>
2014-11-23 23:29:06 +0530

    get text, code from object body

--- nidaba/core/objects.py
+++ nidaba/core/objects.py
@@ -1,3 +1,32 @@
+from pyparsing import makeHTMLTags, SkipTo
+from html.parser import HTMLParser
+
+
+class MLStripper(HTMLParser):
+    """
+    Simple parser to strip HTML tags.
+    :param: None
+    :return: None
+    """
+
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.fed = []
+
+    def handle_data(self, d):
+        self.fed.append(d)
+
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+
 class Comment(object):
     """
     Stack Overflow Comment object which will hold information for use in Nidaba analysis.
@@ -40,6 +69,9 @@ class Question(object):
         """
 
         self._data = data
+        self.body = data.get('Body', '')
+        self.text = strip_tags(self.body)
+        self.code = self._get_code(self.body)
 
         if answers is None:
             self.answers = []
@@ -50,6 +82,12 @@ class Question(object):
             self.comments = []
         else:
             self.comments = [Comment(comm) for comm in comments]
+
+    @classmethod
+    def _get_code(cls, html):
+        code_start, code_end = makeHTMLTags('code')
+        code = code_start + SkipTo(code_end).setResultsName('body') + code_end
+        return ''.join(token.body for token, start, end in code.scanString(html))
 
 
 class User(object):
