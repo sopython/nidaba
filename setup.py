7525c39b7c37bde499a686310070c32a86c1ccc7
chillaranand <anand21nanda@gmail.com>
2014-11-23 20:05:35 +0530

    added classifiers to setup file

--- setup.py
+++ setup.py
@@ -11,7 +11,26 @@ setup(
     author='Keiron Pizzey',
     author_email='ffisegydd@sopython.com',
     description='Machine learning library for studying Stack Overflow data.',
-    install_requires=requirements
-    )
-    
-    
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+
+        'Operating System :: OS Independent',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3 :: Only',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+
+        'Topic :: Internet',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
