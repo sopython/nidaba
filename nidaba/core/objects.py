272d6ce2ab37a136ca78053db370c54ff2bc57a5
Ffisegydd <kjpizzey@gmail.com>
2014-11-24 12:13:50 +0000

    Moved functions to parser.py
    Moved the HTML parsing functions from objects.py to parser.py and imported them into objects.py

--- nidaba/core/objects.py
+++ nidaba/core/objects.py
@@ -1,31 +1,6 @@
 from pyparsing import makeHTMLTags, SkipTo
-from html.parser import HTMLParser
 
-
-class MLStripper(HTMLParser):
-    """
-    Simple parser to strip HTML tags.
-    :param: None
-    :return: None
-    """
-
-    def __init__(self):
-        self.reset()
-        self.strict = False
-        self.convert_charrefs= True
-        self.fed = []
-
-    def handle_data(self, d):
-        self.fed.append(d)
-
-    def get_data(self):
-        return ''.join(self.fed)
-
-def strip_tags(html):
-    s = MLStripper()
-    s.feed(html)
-    return s.get_data()
-
+from .parser import strip_tags
 
 class Comment(object):
     """
---
+++ nidaba/core/parser.py
@@ -0,0 +1,27 @@
+from html.parser import HTMLParser
+
+
+class MLStripper(HTMLParser):
+    """
+    Simple parser to strip HTML tags.
+    :param: None
+    :return: None
+    """
+
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs = True
+        self.fed = []
+
+    def handle_data(self, d):
+        self.fed.append(d)
+
+    def get_data(self):
+        return ''.join(self.fed)
+
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
