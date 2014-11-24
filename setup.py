b13419fa3d8319a9ec89262a06f8a40458e94520
Ffisegydd <kjpizzey@gmail.com>
2014-11-24 13:12:59 +0000

    Closes #20
    Added support for tox. Installing tox (via pip install tox) and then running the command `tox` at the top level will create a separate venv and search through the entire nidaba project for py.test tests to run.

--- setup.py
+++ setup.py
@@ -1,7 +1,56 @@
-from setuptools import setup, find_packages
+import sys
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+try:
+    with open('requirements.txt') as f:
+        requirements = f.read().splitlines()
+except FileNotFoundError:
+    requirements = []
+
+classifiers = [
+    'Development Status :: 1 - Planning',
+
+    'Operating System :: OS Independent',
+    'Operating System :: POSIX',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: Microsoft :: Windows',
+
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3 :: Only',
+
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+
+    'Topic :: Internet',
+    'Topic :: Scientific/Engineering :: Information Analysis',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 setup(
     name='nidaba',
@@ -12,25 +61,5 @@ setup(
     author_email='ffisegydd@sopython.com',
     description='Machine learning library for studying Stack Overflow data.',
     install_requires=requirements,
-    classifiers=[
-        'Development Status :: 1 - Planning',
-
-        'Operating System :: OS Independent',
-        'Operating System :: POSIX',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3 :: Only',
-
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-
-        'Topic :: Internet',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
+    classifiers=classifiers
 )
