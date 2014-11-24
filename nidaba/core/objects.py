7b9a05c4d73270c25727d800edf7055463d8eddc
chillaranand <anand21nanda@gmail.com>
2014-11-24 18:23:22 +0530

    strip_tags should return list instead of str

--- nidaba/core/objects.py
+++ nidaba/core/objects.py
@@ -18,13 +18,10 @@ class MLStripper(HTMLParser):
     def handle_data(self, d):
         self.fed.append(d)
 
-    def get_data(self):
-        return ''.join(self.fed)
-
 def strip_tags(html):
     s = MLStripper()
     s.feed(html)
-    return s.get_data()
+    return s.fed
 
 
 class Comment(object):
